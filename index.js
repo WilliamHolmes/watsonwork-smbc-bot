5d9714530d57ec5bc66709be128bd2f59bc5992b
William Holmes <holmesw@ie.ibm.com>
2018-07-12 22:43:39 +0100

    add new package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const _ = require('underscore');
 // const del = require('delete');
 const got = require('got');
+const metascraper = require('metascraper');
 
 // const fs = require('fs');
 // const request = require('request');
@@ -16,10 +17,10 @@ const UI = require('watsonworkspace-sdk').UI;
 app.authenticate().then(() => app.uploadPhoto('./appicon.jpg'));
 
 app.on('message-created', message => {
-    const { content = '', spaceId } = message;
+    const { content = '' } = message;
     _.each(content.match(constants.regex.SMBC), targetUrl => {
-        const {body: html, url} = await got(targetUrl)
-        const metadata = await metascraper({ html, url })
+        const { body: html, url } = await got(targetUrl);
+        const metadata = await metascraper({ html, url });
         const { image } = metadata;
         console.log('IMAGE', image);
     });
