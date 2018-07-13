3d479ec73b99b3c28c80ca7de433e3556a30b54e
William Holmes <holmesw@ie.ibm.com>
2018-07-13 09:25:17 +0100

    Update packages

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const _ = require('underscore');
-// const del = require('delete');
 const scrape = require('html-metadata');
-
-// const fs = require('fs');
-// const request = require('request');
 
 const appFramework = require('watsonworkspace-bot');
 appFramework.level('verbose');
@@ -12,16 +8,13 @@ const app = appFramework.create();
 
 const UI = require('watsonworkspace-sdk').UI;
 
-
 app.authenticate().then(() => app.uploadPhoto('./appicon.jpg'));
 
 app.on('message-created', message => {
     console.log('message-created', message);
     const { content = '' } = message;
     _.each(content.match(constants.regex.SMBC), url => {
-        scrape(url, (err, data) => console.log('IMAGE', err, data));
+        console.log('URL', url);
+        scrape(url).then(data => console.log('META', data));
     });
 });
-
-// app.on('actionSelected', (message, annotation) => {
-// });
